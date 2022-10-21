94ef26f1c12f49874e1f448a7a6b9f3479d0899e
skyhanull <anky0425@naver.com>
2022-10-21 15:32:27 +0900

    dustmq

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React d
+          Learn React d dkssudgktpdy
         </a>
       </header>
     </div>
